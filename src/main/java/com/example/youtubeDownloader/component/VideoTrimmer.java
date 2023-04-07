aed848143548b1b1de7a275cffa87195b4a0f126
Jakub Neidl <jakub.neidl@gmail.com>
2023-04-07 13:11:12 +0200

    experiment

--- src/main/java/com/example/youtubeDownloader/component/VideoTrimmer.java
+++ src/main/java/com/example/youtubeDownloader/component/VideoTrimmer.java
@@ -24,47 +24,18 @@ public class VideoTrimmer {
 
         long duration = endTimeSeconds - startTimeSeconds;
 
-        FFmpeg.atPath(Path.of(getFFmpegExecutablePath()))
+        FFmpeg.atPath()
                 .addInput(
                         UrlInput.fromPath(inputPath)
                                 .setPosition(startTimeSeconds, TimeUnit.SECONDS)
                                 .setDuration(duration, TimeUnit.SECONDS)
                 )
-                .addArguments("-movflags", "faststart")
                 .setOverwriteOutput(true)
                 .addOutput(
                         UrlOutput.toPath(outputPath)
                 )
                 .execute();
     }
-
-    private String getFFmpegExecutablePath() {
-        String os = System.getProperty("os.name").toLowerCase();
-        String resourceName;
-
-        if (os.contains("win")) {
-            resourceName = "/ffmpeg-binaries/windows";
-        } else if (os.contains("linux") || os.contains("nix") || os.contains("nux") || os.contains("aix")) {
-            resourceName = "/ffmpeg-binaries/linux";
-        } else {
-            throw new UnsupportedOperationException("Unsupported operating system: " + os);
-        }
-
-        try {
-            URL resourceUrl = getClass().getResource(resourceName);
-            if (resourceUrl == null) {
-                throw new FileNotFoundException("FFmpeg binary not found for the current operating system.");
-            }
-
-            File ffmpegFile = new File(resourceUrl.toURI());
-            return ffmpegFile.getAbsolutePath();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Error converting URL to URI.", e);
-        } catch (IOException e) {
-            throw new RuntimeException("Error finding FFmpeg executable.", e);
-        }
-    }
-
 }
 
 
